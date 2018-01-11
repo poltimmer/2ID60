66fdcb51f144e9c31ce57d535826f26cff96fb57
Dağkılıç <a.dagkilic@student.tue.nl>
2018-01-11 11:54:14 +0100

    saving

--- blog/views.py
+++ blog/views.py
@@ -75,5 +75,14 @@ def upload2(request):
     return HttpResponseForbidden('allowed only via POST')
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})

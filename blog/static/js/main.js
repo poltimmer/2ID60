a30910c833c16b307b86abb3e383b360600cbef5
SHypermedia <j.y.nieuwkoop@student.tue.nl>
2018-01-11 20:52:11 +0100

    idk

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -61,6 +61,16 @@
         $(this).text("Follow");
     };
   });
+  
+    
+  const $dropdown1 = $('.dropdown1');
+  const $Adropdown = $('#Adropdown');
+
+  $("#dropdown1").on('click', () => {
+    $(".Adropdown").toggle();
+    $(".fa-chevron-down").addclass("fa-rotate-90");
+    //$("#dropdown1").toggleClass(fa-rotate-180);
+  });
 
 
 

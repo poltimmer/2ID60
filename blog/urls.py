708293d84c11a0a8a7dfa9550491544622be31da
poltimmer <pol.timmer@gmail.com>
2018-01-12 22:36:12 +0100

    changed some urls, added @login_required.
    /newUser/ is now changed to /myprofile/. the name of the url is still newUser. @login_required is now added to newUser so you can't view newUser without being logged in.

--- blog/urls.py
+++ blog/urls.py
@@ -8,11 +8,11 @@ from blog import views as blog_views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^examples/$', views.examples, name='examples'),
-    url(r'^newUser/$', views.newUser, name='newUser'),
+    url(r'^myprofile/$', views.newUser, name='newUser'),
     url(r'^discover/$', views.discover, name='discover'),
     url(r'^photogallery/$', views.photogallery, name='photogallery'),
     url(r'^jobgallery/$', views.jobgallery, name='jobgallery'),
-    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^simple_upload/$', views.simple_upload, name='simple_upload'),

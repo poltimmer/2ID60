0b957f83fe5efa28c97cbc4f0cd3cb5aab37a947
admin <paulchirita28@yahoo.com>
2018-01-13 13:31:28 +0100

    API works!!

--- api/serializers.py
+++ api/serializers.py
@@ -7,6 +7,6 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         model = Post
-        fields = ('title', 'text', 'date_modified', 'skills', 'tags', 'jobType', 'price')
+        fields = ('title', 'text', 'skills', 'tags', 'jobType', 'price')
         read_only_fields = ('creator', 'created_date', 'published_date', 'postType', 'downloads')
 # Views
--- api/views.py
+++ api/views.py
@@ -7,10 +7,6 @@ class CreateView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-    def perform_create(self, serializer):
-        """Save the POST data when creating a new post."""
-        serializer.save()
 # Create your views here.
 
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):

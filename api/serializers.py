241d86e7062d830ef5a054917e6a26cea2f68d1f
admin <paulchirita28@yahoo.com>
2018-01-15 12:13:34 +0100

    api update

--- api/serializers.py
+++ api/serializers.py
@@ -7,6 +7,6 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         model = Post
-        fields = ('id', 'title','author', 'created_date', 'published_date', 'downloads', 'img', 'price', 'tag')
-        read_only_fields = ('id', 'title','author', 'created_date', 'published_date', 'downloads', 'img', 'price', 'tag')
+        fields = ('id', 'title','author', 'created_date', 'img', 'price', 'tag')
+        read_only_fields = ('id', 'title','author', 'created_date', 'img', 'price', 'tag')
 # Views
